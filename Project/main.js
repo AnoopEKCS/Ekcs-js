72482ba2f1c5d44bc58e5d1dd7a7e8a9ebf044dc
AnoopEKCS <akupadhyay@ekcs.co>
2022-12-31 12:31:52 +0530

    update

--- Project/main.js
+++ Project/main.js
@@ -7,3 +7,19 @@ function allCap(){
 
     text.value = upper // UPPER CASE
 }
+
+function allLow(){
+    var text = document.getElementById("text"); // textarea
+    
+    var inputValue = text.value; // textarea.value
+
+    var lower = inputValue.toLowerCase(); // Lower CASE
+
+    text.value = lower // Lower CASE
+}
+
+
+function copy(){
+    var text = document.getElementById("text").value; // textarea
+    navigator.clipboard.writeText(text)
+}
